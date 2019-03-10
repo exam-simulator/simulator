0b8f0780990c8d854d71140968c1f6b69027e15e
benjaminadk <benjaminadk@gmail.com>
2019-03-10 10:36:29 -0700

    :sparkles: Add calculator feature - windows only

--- src/renderer/components/Navigation/Footer/ExamFooter.js
+++ src/renderer/components/Navigation/Footer/ExamFooter.js
@@ -5,14 +5,16 @@ import { Timer } from 'styled-icons/material/Timer'
 import { SkipPrevious } from 'styled-icons/material/SkipPrevious'
 import { KeyboardArrowRight } from 'styled-icons/material/KeyboardArrowRight'
 import { KeyboardArrowLeft } from 'styled-icons/material/KeyboardArrowLeft'
+import { Calculator } from 'styled-icons/icomoon/Calculator'
 import { SkipNext } from 'styled-icons/material/SkipNext'
+import { execFile } from 'child_process'
 import formatTimer from '../../../utils/formatTimer'
 
 const ExamFooter = styled.div`
   width: ${props => (props.open ? 'calc(100% - 24rem)' : 'calc(100% - 5rem)')};
   height: 100%;
   display: grid;
-  grid-template-columns: 15rem 1fr 15rem;
+  grid-template-columns: 10rem 1fr 5rem;
   align-items: center;
   transition: 0.3s;
   .timer {
@@ -33,22 +35,28 @@ const ExamFooter = styled.div`
     justify-self: center;
     display: grid;
     grid-template-columns: repeat(4, 5rem);
-    .arrow {
-      height: 5rem;
-      display: grid;
-      justify-items: center;
-      align-items: center;
-      transition: 0.3s;
-      cursor: pointer;
-      &:hover {
-        background: ${props => lighten(0.1, props.theme.primary)};
-      }
-      svg {
-        color: ${props => props.theme.black};
-      }
+  }
+  .arrow {
+    height: 5rem;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    transition: 0.3s;
+    cursor: pointer;
+    &:hover {
+      background: ${props => lighten(0.1, props.theme.primary)};
+    }
+    svg {
+      color: ${props => props.theme.black};
     }
   }
 `
+
+const calculator = () => {
+  if (process.platform === 'win32') {
+    execFile(`C:/Windows/System32/calc.exe`)
+  }
+}
 
 export default ({
   open,
@@ -77,6 +85,8 @@ export default ({
         <SkipNext size={30} />
       </div>
     </div>
-    <div />
+    <div className="arrow" onClick={calculator}>
+      <Calculator size={30} />
+    </div>
   </ExamFooter>
 )
